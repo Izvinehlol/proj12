5f64d233f9faa23b44699ffdc5ac30724a63a31f
vinicius <vinicius.a.silva14@edu.senai.br>
2026-05-26 09:18:39 -0300

    feat: Exercício 1 concluído

---
+++ ex01/script.js
@@ -0,0 +1,7 @@
+let nomes = ["Vinicius", "Isabelly", "Lívia", "Miguel", "Maria", "Isabella", "Wesley"];
+
+console.log("Nomes em ordem inversa:");
+
+for (let i = 6; i >= 0; i--) {
+    console.log(nomes[i]);
+}
